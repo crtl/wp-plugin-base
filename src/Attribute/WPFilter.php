0e020c9bc8824d4d7c75743e8c85884b35ba3264
crtl <mp@borntocreate.de>
2024-01-17 15:53:04 +0100

    Makes WPFilter repeatable

--- src/Attribute/WPFilter.php
+++ src/Attribute/WPFilter.php
@@ -2,6 +2,6 @@
 
 namespace Crtl\WpPluginBase\Attribute;
 
-#[\Attribute(\Attribute::TARGET_METHOD)]
+#[\Attribute(\Attribute::TARGET_METHOD | \Attribute::\IS_REPEATABLE)]
 class WPFilter extends WPHook
 {}
